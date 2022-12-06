d809d56c5d85a157baf4b6859ca61059fcb9538e
SONG-2J <2039274074@qq.com>
2022-12-06 11:04:28 +0800

    day2-实现分区统计

--- bikeProcess/src/main/scala/processHiveData.scala
+++ bikeProcess/src/main/scala/processHiveData.scala
@@ -23,36 +23,36 @@ object processHiveData {
       .enableHiveSupport().getOrCreate()
 
     // selectSimpleTemp(sparkSession,Array("*"),"instant","instant asc").show(5)
-    // 天气统计
-    var dataframe = selectSimpleTemp(sparkSession, Array("weathersit", "sum(cnt) weathersit_cnt"), "weathersit", "weathersit asc")
-    dataframe2Mysql(dataframe,"weathersit_cnt")
+    // cnt分区统计
+    useRate(sparkSession)
   }
+
   // 全部放一个函数里
-  def oneStep(sparkSession: SparkSession): Unit ={
+  def oneStep(sparkSession: SparkSession): Unit = {
     // 按季节分组统计
     var dataframe = selectSimpleTemp(sparkSession, Array("season", "sum(cnt) season_cnt"), "season", "season asc")
-    dataframe2Mysql(dataframe,"season_sum")
+    dataframe2Mysql(dataframe, "season_sum")
     // 按季节分组均值
     dataframe = selectSimpleTemp(sparkSession, Array("season", "round(avg(cnt),2) season_avg"), "season", "season asc")
-    dataframe2Mysql(dataframe,"season_avg")
+    dataframe2Mysql(dataframe, "season_avg")
     // 年份统计
     dataframe = selectSimpleTemp(sparkSession, Array("yr", "sum(cnt) yr_cnt"), "yr", "yr asc")
-    dataframe2Mysql(dataframe,"yr_cnt")
+    dataframe2Mysql(dataframe, "yr_cnt")
     // 月份统计
     dataframe = selectSimpleTemp(sparkSession, Array("mnth", "sum(cnt) mnth_cnt"), "mnth", "mnth asc")
-    dataframe2Mysql(dataframe,"mnth_cnt")
+    dataframe2Mysql(dataframe, "mnth_cnt")
     // 月份平均
     dataframe = selectSimpleTemp(sparkSession, Array("mnth", "round(avg(cnt),2) mnth_avg"), "mnth", "mnth asc")
-    dataframe2Mysql(dataframe,"mnth_avg")
+    dataframe2Mysql(dataframe, "mnth_avg")
     // 假期平均
     dataframe = selectSimpleTemp(sparkSession, Array("holiday", "round(avg(cnt),2) holiday_avg"), "holiday", "holiday asc")
-    dataframe2Mysql(dataframe,"holiday_avg")
+    dataframe2Mysql(dataframe, "holiday_avg")
     // 星期平均
     dataframe = selectSimpleTemp(sparkSession, Array("weekday", "round(avg(cnt),2) week_avg"), "weekday", "weekday asc")
-    dataframe2Mysql(dataframe,"week_avg")
+    dataframe2Mysql(dataframe, "week_avg")
     // 天气统计
     dataframe = selectSimpleTemp(sparkSession, Array("weathersit", "sum(cnt) weathersit_cnt"), "weathersit", "weathersit asc")
-    dataframe2Mysql(dataframe,"weathersit_cnt")
+    dataframe2Mysql(dataframe, "weathersit_cnt")
   }
 
   // 查询模版
@@ -72,5 +72,36 @@ object processHiveData {
       .option("user", MYSQL_USER)
       .option("password", MYSQL_PWD)
       .save()
+  }
+
+
+  // cnt分区
+  def cntSection(sparkSession: SparkSession): Unit = {
+    sparkSession.sql("select cnt from " + SELECT_DB+"."+SELECT_TABLE).createOrReplaceTempView("v1")
+    sparkSession.sql("select case " +
+      "when cnt <= 1000 then '0~1000' " +
+      "when cnt <= 2000 then '1000~2000' " +
+      "when cnt <= 3000 then '2000~3000' " +
+      "when cnt <= 4000 then '3000~4000' " +
+      "when cnt <= 5000 then '4000~5000' " +
+      "when cnt <= 6000 then '5000~6000' " +
+      "else '6000+' end cnt_section from v1").createOrReplaceTempView("v2")
+    val dataFrame = sparkSession.sql("select cnt_section,count(cnt_section) from v2 group by cnt_section sort by cnt_section")
+    dataFrame.show()
+    dataframe2Mysql(dataFrame,"cnt_section")
+  }
+
+  // 使用百分比统计
+  def useRate(sparkSession: SparkSession): Unit ={
+    sparkSession.sql("select round(registered/cnt,4)*100 use_rate from "+SELECT_DB+"."+SELECT_TABLE).createOrReplaceTempView("v1")
+    sparkSession.sql("select case " +
+      "when use_rate <= 60 then '0~60%' " +
+      "when use_rate <= 70 then '60%~70%' " +
+      "when use_rate <= 80 then '70%~80%' " +
+      "when use_rate <= 90 then '80%~90%' " +
+      "else '90%+' end use_rate from v1").createOrReplaceTempView("v2")
+    val dataFrame = sparkSession.sql("select use_rate,count(use_rate) from v2 group by use_rate order by use_rate")
+    dataFrame.show()
+    dataframe2Mysql(dataFrame,"use_rate")
   }
 }
